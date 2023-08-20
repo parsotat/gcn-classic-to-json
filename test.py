759b62eecfe3a806419fbe5b2033cb2cbdc47fb8
Leo Singer <leo.singer@ligo.org>
2023-08-20 16:59:23 -0400

    Add print statements

--- test.py
+++ test.py
@@ -5,12 +5,16 @@ from gcn_kafka import Consumer
 
 # Connect as a consumer.
 # Warning: don't share the client secret with others.
+print('Creating consumer')
 consumer = Consumer(client_id=os.environ['GCN_CLIENT_ID'],
                     client_secret=os.environ['GCN_CLIENT_SECRET'],
                     domain=os.environ.get('GCN_DOMAIN'))
 
 # Subscribe to topics and receive alerts
+print('Subscribing')
 consumer.subscribe(['igwn.gwalert'])
+
+print('Entering consume loop')
 while True:
     for message in consumer.consume(timeout=1):
         if message.error():
