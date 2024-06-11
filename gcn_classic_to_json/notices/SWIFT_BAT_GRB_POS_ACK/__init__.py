00047dec23fd310e6825cfbafbe0d8c1be202434
Leo Singer <leo.singer@ligo.org>
2024-06-11 15:07:02 -0400

    Add trigger time

--- gcn_classic_to_json/notices/SWIFT_BAT_GRB_POS_ACK/__init__.py
+++ gcn_classic_to_json/notices/SWIFT_BAT_GRB_POS_ACK/__init__.py
@@ -1,2 +1,16 @@
+from astropy.time import Time
+
+# Zero point for Truncated Julian Day according to
+# https://en.wikipedia.org/wiki/Julian_day.
+TJD0 = (2440000, 0.5)
+
+
 def parse(bin):
-    return dict(ra=1e-4 * bin[7], dec=1e-4 * bin[8])
+    return dict(
+        trigger_time=Time(
+            bin[5] + TJD0[0], bin[6] / 8640000 + TJD0[1], format="jd",
+        ).isot,
+        ra=1e-4 * bin[7],
+        dec=1e-4 * bin[8],
+        ra_dec_error=1e-4 * bin[11],
+    )
