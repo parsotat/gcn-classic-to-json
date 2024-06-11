b1960149d6db1cffa25f544b08611b76cad1174b
Leo Singer <leo.singer@ligo.org>
2024-06-11 15:14:04 -0400

    Add trigger_time

--- gcn_classic_to_json/notices/__init__.py
+++ gcn_classic_to_json/notices/__init__.py
@@ -1,8 +1,8 @@
 import importlib
 import pkgutil
-import struct
 
-_int4 = struct.Struct("!l")
+import numpy as np
+
 _parsers = {
     module: importlib.import_module(f".{module}", __package__).parse
     for _, module, _ in pkgutil.iter_modules(__path__)
@@ -12,7 +12,7 @@ keys = tuple(_parsers.keys())
 
 
 def parse(key, value):
-    ints = [_ for _, in _int4.iter_unpack(value)]
+    ints = np.frombuffer(value, dtype=">i4")
     assert len(ints) == 40
     parser = _parsers[key]
     return parser(ints)
