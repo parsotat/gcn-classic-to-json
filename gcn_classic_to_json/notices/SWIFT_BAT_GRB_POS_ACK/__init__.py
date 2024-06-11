0a80426e503b8cfd1deaf4ee9cd265daf4ccb46b
Leo Singer <leo.singer@ligo.org>
2024-06-11 15:13:26 -0400

    Run Black

--- gcn_classic_to_json/notices/SWIFT_BAT_GRB_POS_ACK/__init__.py
+++ gcn_classic_to_json/notices/SWIFT_BAT_GRB_POS_ACK/__init__.py
@@ -8,7 +8,9 @@ TJD0 = (2440000, 0.5)
 def parse(bin):
     return dict(
         trigger_time=Time(
-            bin[5] + TJD0[0], bin[6] / 8640000 + TJD0[1], format="jd",
+            bin[5] + TJD0[0],
+            bin[6] / 8640000 + TJD0[1],
+            format="jd",
         ).isot,
         ra=1e-4 * bin[7],
         dec=1e-4 * bin[8],
