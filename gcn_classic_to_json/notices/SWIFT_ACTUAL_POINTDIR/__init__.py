64a84b304a84e699539c3bf8ff7ab3219440d164
Tyler Parsotan <parsotat@umbc.edu>
2024-06-28 10:27:01 -0400

    Copied the the SWIFT_BAT_GRB_POS_ACK __init__.py template to the other swift notices directories for modification.

---
+++ gcn_classic_to_json/notices/SWIFT_ACTUAL_POINTDIR/__init__.py
@@ -0,0 +1,32 @@
+"""
+This is the documentation for this packet from GCN classic for ease of reference:
+
+------Insert Documentation Here-------
+"""
+
+from astropy.time import Time
+
+#set constants for ease of use
+
+# Zero point for Truncated Julian Day according to
+# https://en.wikipedia.org/wiki/Julian_day.
+TJD0 = (2440000, 0.5)
+
+mission="Swift"
+instrument=None #insert instrument here for appropriate notice type
+
+
+def parse(bin):
+    packet_dict=dict(
+        mission=mission,
+        instrument=instrument,
+        trigger_time=Time(
+            bin[5] + TJD0[0],
+            bin[6] / 8640000 + TJD0[1],
+            format="jd",
+        ).isot,
+        ra=1e-4 * bin[7],
+        dec=1e-4 * bin[8],
+        ra_dec_error=1e-4 * bin[11],
+    )
+    return packet_dict
