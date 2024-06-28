c68e51e599ad90ff6d38084fef78e39042f9c36b
Tyler Parsotan <parsotat@umbc.edu>
2024-06-28 10:15:31 -0400

    Added additional template code to the SWIFT_BAT_GRB_POS_ACK __init__.py file for ease of documentation and transferring code to other swift notices.

--- gcn_classic_to_json/notices/SWIFT_BAT_GRB_POS_ACK/__init__.py
+++ gcn_classic_to_json/notices/SWIFT_BAT_GRB_POS_ACK/__init__.py
@@ -1,12 +1,25 @@
+"""
+This is the documentation for this packet from GCN classic for ease of reference:
+
+------Insert Documentation Here-------
+"""
+
 from astropy.time import Time
+
+#set constants for ease of use
 
 # Zero point for Truncated Julian Day according to
 # https://en.wikipedia.org/wiki/Julian_day.
 TJD0 = (2440000, 0.5)
 
+mission="Swift"
+instrument="BAT"
+
 
 def parse(bin):
-    return dict(
+    packet_dict=dict(
+        mission=mission
+        instrument=instrument
         trigger_time=Time(
             bin[5] + TJD0[0],
             bin[6] / 8640000 + TJD0[1],
@@ -16,3 +29,4 @@ def parse(bin):
         dec=1e-4 * bin[8],
         ra_dec_error=1e-4 * bin[11],
     )
+    return packet_dict
