ee1afa602e34dc6ab228e568ec6efe2051983702
Tyler Parsotan <parsotat@umbc.edu>
2025-08-13 13:28:41 -0400

    The UVOT image binning can have different levels besides what is in the documentation, not sure why, but based on the logs the actual binning is stil 4096 so corrected this in the parsing function.

--- gcn_classic_to_json/notices/SWIFT_UVOT_DBURST_PROC/__init__.py
+++ gcn_classic_to_json/notices/SWIFT_UVOT_DBURST_PROC/__init__.py
@@ -31,7 +31,7 @@ def parse_uvot_image(bin):
         "grb_position": [x_pos, y_pos],
         "n_frames": bin[17],
         "fits_file_url": f"http://gcn.gsfc.nasa.gov/gcn/notices_s/{''.join([i.decode('utf-8') for i in  bin[22:39].view('c')])}",
-        "pixel_binning": pixel_binning_values[pixel_binning[0]],
+        "pixel_binning": pixel_binning_values[pixel_binning[0]] if pixel_binning[0] in pixel_binning_values.keys() else pixel_binning_values[6],
         "bright_star_nearby": bool(misc_bits[13]),
         "was_subthresh": bool(misc_bits[20]),
         "grb_position_source": grb_position_sources[misc_bits[28]],
